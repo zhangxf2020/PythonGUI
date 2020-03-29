663b018f2f13c024f15558dd30d8e5fead4ea85e
zhangxf2020 <fan102684@163.com>
2020-03-29 14:38:31 +0800

    Revert "添加文件"
    This reverts commit ccb1bafe

--- Demo3_9TreeDock/myMainWidget.py
+++ Demo3_9TreeDock/myMainWidget.py
@@ -1,8 +1,18 @@
-from PyQt5.QtWidgets import QMainWindow,QApplication,QTreeWidget,QLabel,QAction,QDockWidget,QMenu
-from PyQt5.QtCore import pyqtSlot,Qt
-
+from PyQt5.QtWidgets import QMainWindow,QApplication,QLabel,QDockWidget,QMenu,QTreeWidgetItem,QFileDialog
+from PyQt5.QtGui import QIcon,QPixmap
+from PyQt5.QtCore import pyqtSlot,Qt,QDir,QFileInfo
+from enum import Enum
 import sys
 from ui_MainWidget import Ui_MainWindow
+
+class TreeItemType(Enum):
+    itTopItem = 1001
+    itGroupItem = 1002
+    itImageItem = 1003
+
+class TreeColNum(Enum):
+    colItem = 0
+    colItemType = 1
 
 class QmyMainWidget(QMainWindow):
     def __init__(self,parent=None):
@@ -18,12 +28,23 @@ class QmyMainWidget(QMainWindow):
         self.ui.scrollArea.setWidgetResizable(True)#设置自动滚动区控件的尺寸为自适应
         self.ui.scrollArea.setAlignment(Qt.AlignCenter)#设置自动滚动区域对其方式,居中
         self.ui.LabPicture.setAlignment(Qt.AlignCenter)#设置图片标签控件对其方式,居中
+
+        self.itemFlags = Qt.ItemIsSelectable | Qt.ItemIsUserCheckable | Qt.ItemIsEnabled | Qt.ItemIsAutoTristate
         self.__initTree()
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~优雅的分割线~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     #初始化目录树
     def __initTree(self):
-        pass
+        self.ui.treeFiles.clear()
+        icon = QIcon(r':icons/icons/15.ico')
+
+        item = QTreeWidgetItem(TreeItemType.itTopItem.value)
+        item.setIcon(TreeColNum.colItem.value,icon)
+        item.setText(TreeColNum.colItem.value,'图片文件')
+        item.setFlags(self.itemFlags)
+        item.setCheckState(TreeColNum.colItem.value,Qt.Checked)
+        item.setData(TreeColNum.colItem.value,Qt.UserRole,'')
+        self.ui.treeFiles.addTopLevelItem(item)
 
 
 if __name__ == '__main__':
